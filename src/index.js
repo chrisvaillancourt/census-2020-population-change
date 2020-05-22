61e4c5a27dc210ad16aa2e146e5e963e99bb6499
Chris Vaillancourt <cvaillan22@gmail.com>
2020-05-22 12:53:21 -0700

    basic map with no special config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 'use strict';
+var l = console.log.bind(console);
+l('loading...');
+
+require(['esri/Map', 'esri/views/MapView'], function (Map, MapView) {
+  var map = new Map({
+    basemap: 'topo-vector',
+  });
+
+  var view = new MapView({
+    container: 'viewDiv',
+    map: map,
+    center: [-118.805, 34.027], // longitude, latitude
+    zoom: 13,
+  });
+});
