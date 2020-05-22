fd85b0aad00fec24bfc967e5da9f5d07c0d6c30a
Chris Vaillancourt <cvaillan22@gmail.com>
2020-05-22 15:40:23 -0700

    removed tracking output

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import './styles/style.css';
+import esriMap from 'esri/Map';
+import MapView from 'esri/views/MapView';
+
 var l = console.log.bind(console);
-l('loading...');
-
-require(['esri/Map', 'esri/views/MapView'], function (Map, MapView) {
-  var map = new Map({
-    basemap: 'topo-vector',
-  });
-
-  var view = new MapView({
-    container: 'viewDiv',
-    map: map,
-    center: [-118.805, 34.027], // longitude, latitude
-    zoom: 13,
-  });
+console.time('map');
+var map = new esriMap({
+  basemap: 'topo-vector',
 });
+
+var view = new MapView({
+  container: 'viewDiv',
+  map: map,
+  center: [-118.805, 34.027], // longitude, latitude
+  zoom: 13,
+});
+
+console.timeEnd('map');
