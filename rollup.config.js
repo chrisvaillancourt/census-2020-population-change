60cf59773549e1607cb58ae9ac4baddbb7165c0e
Chris Vaillancourt <cvaillan22@gmail.com>
2020-06-11 16:50:10 -0700

    initial setup for pop2020 map

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
 import copy from 'rollup-plugin-copy';
 import del from 'rollup-plugin-delete';
-import livereload from 'rollup-plugin-livereload';
-import postcss from 'rollup-plugin-postcss';
+import browsersync from 'rollup-plugin-browsersync';
+import styles from 'rollup-plugin-styles';
 import serve from 'rollup-plugin-serve';
 import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
@@ -13,9 +13,10 @@ const OUTPUT =
   process.env.BUILD === 'development' ? 'output/dev' : 'output/dist';
 const SOURCEMAP = process.env.BUILD === 'development' ? 'inline' : false;
 const MINIFY = process.env.BUILD === 'development' ? null : terser();
+const minifyCss = process.env.BUILD === 'development' ? false : true;
 const SERVER =
   process.env.BUILD === 'development'
-    ? [serve(OUTPUT), livereload(OUTPUT)]
+    ? [serve(OUTPUT), browsersync({ server: OUTPUT })]
     : [];
 
 export default {
@@ -24,17 +25,22 @@ export default {
     dir: OUTPUT,
     format: 'amd',
     sourcemap: SOURCEMAP,
+    entryFileNames: '[name].js',
+    assetFileNames: '[name][extname]',
   },
   plugins: [
+    del({ targets: OUTPUT }),
     resolve(),
     babel({ babelHelpers: 'bundled' }),
-    del({ targets: OUTPUT }),
-    MINIFY,
-    postcss({
-      extensions: ['.css'],
-      extract: true,
-      minimize: true,
+    styles({
+      mode: 'extract',
+      minimize: minifyCss,
+      url: {
+        publicPath: '../assets/',
+      },
     }),
+    MINIFY,
+
     copy({
       targets: [{ src: 'public/*', dest: OUTPUT }],
     }),
