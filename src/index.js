2126d5922eb5d681a51d1adff81d450e9068b35e
Chris Vaillancourt <cvaillan22@gmail.com>
2020-06-12 12:38:57 -0700

    added local geo data

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import './styles/normalize.css';
 import './styles/style.css';
 import esriMap from 'esri/Map';
 import MapView from 'esri/views/MapView';
-import { basemap } from './data/basemap';
+import { basemap } from './js/basemap';
 import Home from 'esri/widgets/Home';
-import { countyLayer } from './data/featureLayers';
-var l = console.log.bind(console);
+import { countyGeoJson } from './js/featureLayers';
 
+console.time('map');
 async function createMap() {
-  console.time('map');
+  var countyLayer = await countyGeoJson;
+
   var map = new esriMap({
     basemap,
     layers: [countyLayer],
@@ -17,8 +18,6 @@ async function createMap() {
   var view = new MapView({
     container: 'viewDiv',
     map: map,
-    center: [-99.74405, 38.1374], // longitude, latitude
-    zoom: 3,
   });
   var homeBtn = new Home({
     view,
@@ -27,12 +26,19 @@ async function createMap() {
   view.ui.move('zoom', 'bottom-right');
 
   await view.when();
+
   view.ui.add([homeBtn], 'bottom-right');
+
   var countyLayerView = await view.whenLayerView(countyLayer);
+
   countyLayerView.watch('updating', async (isUpdating) => {
     if (isUpdating) return;
+
+    var layerExtent = await countyLayerView.queryExtent();
+    view.goTo(layerExtent);
     var results = await countyLayerView.queryFeatures();
     var attributes = results.features.map((feature) => feature.attributes);
+
     console.log(attributes);
   });
 }
---
+++ src/js/featureLayers.js
@@ -0,0 +1,30 @@
+import FeatureLayer from 'esri/layers/FeatureLayer';
+import GeoJSONLayer from 'esri/layers/GeoJSONLayer';
+import { json } from 'd3-fetch';
+var countyLayer = new FeatureLayer({
+  url:
+    'https://services7.arcgis.com/Fnhc04BJL93uqCjd/ArcGIS/rest/services/counties_2020_population/FeatureServer/1',
+  outFields: ['*'],
+});
+
+async function createEsriGeoJson(geoJson) {
+  var blob = await new Blob([JSON.stringify(geoJson)], {
+    type: 'application/json',
+  });
+  var url = URL.createObjectURL(blob);
+  var geoJsonLayer = new GeoJSONLayer({
+    url,
+  });
+  return geoJsonLayer;
+}
+async function createCountyGeoJsonLyr() {
+  var countyGeoJson = await json(
+    './geo-data/county-json/county_simplified_contig_albers.json'
+  );
+  var countyEsriGeoJson = await createEsriGeoJson(countyGeoJson);
+  return countyEsriGeoJson;
+}
+
+var countyGeoJson = createCountyGeoJsonLyr();
+
+export { countyLayer, countyGeoJson };
