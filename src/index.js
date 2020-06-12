e0e06a82ae2026822c2d489a1deec41211d3a508
Chris Vaillancourt <cvaillan22@gmail.com>
2020-06-11 17:37:16 -0700

    added layerview querying

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import esriMap from 'esri/Map';
 import MapView from 'esri/views/MapView';
 import { basemap } from './data/basemap';
 import Home from 'esri/widgets/Home';
-
+import { countyLayer } from './data/featureLayers';
 var l = console.log.bind(console);
 
-function createMap() {
+async function createMap() {
   console.time('map');
   var map = new esriMap({
     basemap,
+    layers: [countyLayer],
   });
 
   var view = new MapView({
@@ -24,12 +25,18 @@ function createMap() {
   });
 
   view.ui.move('zoom', 'bottom-right');
-  view
-    .when(async function workWithView() {
-      view.ui.add([homeBtn], 'bottom-right');
-    })
-    .then(() => console.timeEnd('map'))
-    .catch(console.error);
+
+  await view.when();
+  view.ui.add([homeBtn], 'bottom-right');
+  var countyLayerView = await view.whenLayerView(countyLayer);
+  countyLayerView.watch('updating', async (isUpdating) => {
+    if (isUpdating) return;
+    var results = await countyLayerView.queryFeatures();
+    var attributes = results.features.map((feature) => feature.attributes);
+    console.log(attributes);
+  });
 }
 
-createMap();
+createMap()
+  .then(() => console.timeEnd('map'))
+  .catch(console.error);
