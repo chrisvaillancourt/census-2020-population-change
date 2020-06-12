2e3401afbef71f2dc5e8b8d8360fb2ed4c2f59fd
Chris Vaillancourt <cvaillan22@gmail.com>
2020-06-11 17:55:21 -0700

    added all available fields to county layer

--- src/data/featureLayers.js
+++ src/data/featureLayers.js
@@ -3,6 +3,7 @@ import FeatureLayer from 'esri/layers/FeatureLayer';
 var countyLayer = new FeatureLayer({
   url:
     'https://services7.arcgis.com/Fnhc04BJL93uqCjd/ArcGIS/rest/services/counties_2020_population/FeatureServer/1',
+  outFields: ['*'],
 });
 
 export { countyLayer };
