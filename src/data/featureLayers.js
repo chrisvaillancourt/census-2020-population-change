87499351161844c9ddf3a1f8401f140ec77d30e6
Chris Vaillancourt <cvaillan22@gmail.com>
2020-06-11 17:04:34 -0700

    added county feature layer

---
+++ src/data/featureLayers.js
@@ -0,0 +1,8 @@
+import FeatureLayer from 'esri/layers/FeatureLayer';
+
+var countyLayer = new FeatureLayer({
+  url:
+    'https://services7.arcgis.com/Fnhc04BJL93uqCjd/ArcGIS/rest/services/counties_2020_population/FeatureServer/1',
+});
+
+export { countyLayer };
