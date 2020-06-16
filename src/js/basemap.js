33380d8414f8f8ff6cffdfd2b430d7cce1b552bb
Chris Vaillancourt <cvaillan22@gmail.com>
2020-06-16 11:59:59 -0700

    added light and dark gray url variables

--- src/js/basemap.js
+++ src/js/basemap.js
@@ -1,11 +1,17 @@
 import VectorTileLayer from 'esri/layers/VectorTileLayer';
 import Basemap from 'esri/Basemap';
 
-const vectorBaseLayer = new VectorTileLayer({
-  url: `https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/Albers48Basemap/VectorTileServer`,
+const darkGreyUrl =
+  'https://esritech.maps.arcgis.com/sharing/rest/content/items/14463d7a63e64fc48448ca0e269a6c5f/resources/styles/root.json';
+
+const lightGreyUrl =
+  'https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/Albers48Basemap/VectorTileServer';
+
+var vectorBaseLayer = new VectorTileLayer({
+  url: darkGreyUrl,
 });
 
-const basemap = new Basemap({
+var basemap = new Basemap({
   baseLayers: [vectorBaseLayer],
   referenceLayers: [],
 });
