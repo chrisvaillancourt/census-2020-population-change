3075d0288a308f0f4efedd463d4d137e985eb2d2
Chris Vaillancourt <cvaillan22@gmail.com>
2020-06-16 13:10:46 -0700

    added svector state boundary to basemap

--- src/js/basemap.js
+++ src/js/basemap.js
@@ -11,8 +11,13 @@ var vectorBaseLayer = new VectorTileLayer({
   url: darkGreyUrl,
 });
 
+var vectorStateBoundary = new VectorTileLayer({
+  url:
+    'https://tiles.arcgis.com/tiles/Fnhc04BJL93uqCjd/arcgis/rest/services/dtl_st_ln_lower48_albers/VectorTileServer/resources/styles/root.json',
+});
+
 var basemap = new Basemap({
-  baseLayers: [vectorBaseLayer],
+  baseLayers: [vectorBaseLayer, vectorStateBoundary],
   referenceLayers: [],
 });
 
