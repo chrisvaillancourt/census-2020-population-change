420de1c750c2a6c0040faf0553b54ada1811de48
Chris Vaillancourt <cvaillan22@gmail.com>
2020-06-18 10:28:42 -0700

    moved data summary function out of draw chart function

--- src/js/chart/drawChart.js
+++ src/js/chart/drawChart.js
@@ -45,27 +45,6 @@ function drawBarChart(data) {
     'TOTPOP_CY',
   ];
 
-  function sumFields({ fields = [], dataToSummarize = [] } = {}) {
-    // Use a map since it can be more performant than a regular object
-    var summaryDataStore = new Map();
-
-    // set a key for each field we want to summarize
-    fields.forEach(function setStoreKeys(fieldName) {
-      summaryDataStore.set(fieldName, 0);
-    });
-
-    // summarize data with addition
-    dataToSummarize.reduce(function (summaryStore, currentObj) {
-      for (let [key, value] of Object.entries(currentObj)) {
-        if (summaryStore.has(key)) {
-          var newSummaryValue = summaryStore.get(key) + value;
-          summaryStore.set(key, newSummaryValue);
-        }
-      }
-      return summaryStore;
-    }, summaryDataStore);
-    return summaryDataStore;
-  }
   var chartData = sumFields({
     fields: summaryFields,
     dataToSummarize: data,
@@ -131,6 +110,29 @@ function drawBarChart(data) {
   // step 6) draw peripherals
 
   console.timeEnd('draw chart');
+}
+function sumFields({ fields = [], dataToSummarize = [] } = {}) {
+  console.time('summarize fields');
+  // Use a map since it can be more performant than a regular object
+  var summaryDataStore = new Map();
+
+  // set a key for each field we want to summarize
+  fields.forEach(function setStoreKeys(fieldName) {
+    summaryDataStore.set(fieldName, 0);
+  });
+
+  // summarize data with addition
+  dataToSummarize.reduce(function (summaryStore, currentObj) {
+    for (let [key, value] of Object.entries(currentObj)) {
+      if (summaryStore.has(key)) {
+        var newSummaryValue = summaryStore.get(key) + value;
+        summaryStore.set(key, newSummaryValue);
+      }
+    }
+    return summaryStore;
+  }, summaryDataStore);
+  console.timeEnd('summarize fields');
+  return summaryDataStore;
 }
 
 function getDimensions() {
