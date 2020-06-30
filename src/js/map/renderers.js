2822b6bee622b4c1866032db5cfafb0d95d61f38
Chris Vaillancourt <cvaillan22@gmail.com>
2020-06-30 11:27:33 -0700

    refactored renders

--- src/js/map/renderers.js
+++ src/js/map/renderers.js
@@ -1,216 +1,79 @@
-var colorAndSizePurpleAndGreen = {
-  type: 'simple',
-  symbol: {
-    type: 'simple-marker',
-    outline: {
-      color: [255, 255, 255, 0.25],
-      width: '0.5px',
-    },
+/*
+  Previous Color renderers:
+  - blue and gray 3
+  - blue and gray 2
+  - blue and gray 1
+  - 
+
+*/
+
+const baseOpacity = 0.8;
+const outlineColor = 'hsla(48, 8%, 24%, 0.25)';
+var symbolColors = {
+  low: `hsla(37, 75%, 55%, ${baseOpacity})`,
+  medium: `hsla(0, 0%, 20%, ${baseOpacity})`,
+  high: `hsla(171, 55%, 63%, ${baseOpacity})`,
+};
+
+var sizeVisualVariable = {
+  type: 'size',
+  field: 'pop_change',
+  legendOptions: {
+    title: '2010 - 2020 Population Change',
   },
-  label: 'Population 2020 County Centroid',
-  visualVariables: [
+  minDataValue: 0,
+  maxDataValue: 500000,
+  minSize: 4,
+  maxSize: 50,
+};
+var colorVisualVariable = {
+  type: 'color',
+  field: 'pop_growth',
+  legendOptions: {
+    title: '2010 - 2020 Annual Compound Growth Rate',
+  },
+  stops: [
     {
-      type: 'color',
-      field: 'pop_growth',
-      legendOptions: {
-        title: '2010 - 2020 Annual Compound Growth Rate',
-      },
-      stops: [
-        {
-          value: -1.23,
-          color: '#40004b',
-          label: '-1.23%',
-        },
-        {
-          value: 0.76,
-          color: '#f7f7f7',
-          label: '0.76% (US Average)',
-        },
-        {
-          value: 2.26,
-          color: '#00441b',
-          label: '2.26%',
-        },
-      ],
+      value: -0.5,
+      color: symbolColors.low,
+      label: '-1.23%',
     },
+
     {
-      type: 'size',
-      field: 'pop_change',
-      minDataValue: 0,
-      maxDataValue: 200000,
-      minSize: 6,
-      maxSize: 60,
-      // stops: [
-      //   {
-      //     value: 0,
-      //     size: 6,
-      //   },
-      //   {
-      //     value: 200000,
-      //     size: 70,
-      //   },
-      // ],
+      value: 0.76,
+      color: symbolColors.medium,
+      label: '0.76% (US Average)',
+    },
+
+    {
+      value: 2.02,
+      color: symbolColors.high,
+      label: '2.02%',
     },
   ],
 };
 
-var colorAndSizePurpleAndGray1 = {
+var colorAndSizeRenderer = {
   type: 'simple',
   symbol: {
     type: 'simple-marker',
     outline: {
-      color: [255, 255, 255, 0.25],
+      color: outlineColor,
       width: '0.5px',
     },
   },
-  label: 'Population 2020 County Centroid',
-  visualVariables: [
-    {
-      type: 'color',
-      field: 'pop_growth',
-      legendOptions: {
-        title: '2010 - 2020 Annual Compound Growth Rate',
-      },
-      stops: [
-        {
-          value: -1.23,
-          color: '#faefdb',
-          label: '-1.23%',
-        },
-        {
-          value: 0.75,
-          color: '#54534d',
-          // label: '0.76% (Below US Average)',
-        },
-        {
-          value: 0.76,
-          color: '#594154',
-          label: '0.76% (US Average)',
-        },
-        {
-          value: 0.77,
-          color: '#594154',
-          // label: '0.77% (Above US Average)',
-        },
-        {
-          value: 2.26,
-          color: '#ff99ff',
-          label: '2.26%',
-        },
-      ],
-    },
-    {
-      type: 'size',
-      field: 'pop_change',
-      minDataValue: 0,
-      maxDataValue: 200000,
-      minSize: 6,
-      maxSize: 60,
-      // stops: [
-      //   {
-      //     value: 0,
-      //     size: 6,
-      //   },
-      //   {
-      //     value: 200000,
-      //     size: 70,
-      //   },
-      // ],
-    },
-  ],
+  visualVariables: [colorVisualVariable, sizeVisualVariable],
 };
-
-var colorAndSizeBlueAndGray3 = {
-  type: 'simple',
-  symbol: {
-    type: 'simple-marker',
-    outline: {
-      color: [66, 64, 56, 0.25],
-      width: '0.5px',
-    },
-  },
-  visualVariables: [
-    {
-      type: 'color',
-      field: 'pop_growth',
-      legendOptions: {
-        title: '2010 - 2020 Annual Compound Growth Rate',
-      },
-      stops: [
-        {
-          value: -0.5,
-          color: 'hsla(0, 0%, 80%, 0.8)',
-          label: '-1.23%',
-        },
-
-        {
-          value: 0.76,
-          color: 'hsla(48, 8%, 24%, 0.8)',
-          label: '0.76% (US Average)',
-        },
-
-        {
-          value: 2.02,
-          color: 'hsla(198, 100%, 50%, 0.8)',
-          label: '2.02%',
-        },
-      ],
-    },
-    {
-      type: 'size',
-      field: 'pop_change',
-      legendOptions: {
-        title: '2010 - 2020 Population Change',
-      },
-      minDataValue: 0,
-      maxDataValue: 500000,
-      minSize: 4,
-      maxSize: 50,
-    },
-  ],
-};
-var colorBlueAndGray3 = {
+var colorRenderer = {
   type: 'simple',
   symbol: {
     type: 'simple-fill',
     outline: {
-      color: [66, 64, 56, 0.25],
+      color: outlineColor,
       width: '0.5px',
     },
   },
-  visualVariables: [
-    {
-      type: 'color',
-      field: 'pop_growth',
-      legendOptions: {
-        title: '2010 - 2020 Annual Compound Growth Rate',
-      },
-      stops: [
-        {
-          value: -0.5,
-          color: 'hsla(0, 0%, 80%, 0.8)',
-          label: '-1.23%',
-        },
-
-        {
-          value: 0.76,
-          color: 'hsla(48, 8%, 24%, 0.8)',
-          label: '0.76% (US Average)',
-        },
-
-        {
-          value: 2.02,
-          color: 'hsla(198, 100%, 50%, 0.8)',
-          label: '2.02%',
-        },
-      ],
-    },
-  ],
+  visualVariables: [colorVisualVariable],
 };
 
-export {
-  colorAndSizePurpleAndGreen,
-  colorAndSizePurpleAndGray1,
-  colorAndSizeBlueAndGray3,
-  colorBlueAndGray3,
-};
+export { colorAndSizeRenderer, colorRenderer, symbolColors };
